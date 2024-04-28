634e57ca6554db51c42fcf6aa4912b573f8d522f
Miura55 <acordion.piano@gmail.com>
2024-04-28 11:10:21 +0900

    ユーザー名のデフォルト値を修正

--- schemes.py
+++ schemes.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 class Entry(BaseModel):
     title: str = 'Title'
     content: str = 'This is content'
-    user_name: str = 'User' 
+    user_name: str = 'User'
 
 
 class MessageResponse(BaseModel):
