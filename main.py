87143aba6615c1709b2bf1f51f2381e91712846c
Miura55 <acordion.piano@gmail.com>
2024-04-29 03:17:20 +0000

    APIドキュメントを修正

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ from fastapi import FastAPI
 import logging
 import schemes
 
-app = FastAPI()
+app = FastAPI(
+    title="FastAPI Tutorial",
+    summary="このAPIはFastAPIのチュートリアルとして掲示板のAPIを提供します。",
+)
 logger = logging.getLogger('uvicorn')
 
 
-@app.get("/", response_model=schemes.MessageResponse)
+@app.get("/", response_model=schemes.MessageResponse, description="接続確認用のAPIです。")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/entry", response_model=schemes.MessageResponse)
+@app.post("/entry", response_model=schemes.MessageResponse, description="掲示板にエントリーを新規追加します。")
 async def create_entry(entry: schemes.Entry):
     logger.info(f"Received entry: {entry}")
     return {"message": "Entry created"}
 
-@app.delete("/entry/{entry_id}", response_model=schemes.MessageResponse)
+@app.delete("/entry/{entry_id}", response_model=schemes.MessageResponse, description="IDを指定して掲示板のエントリーを削除します。")
 async def delete_entry(entry_id: int):
     logger.info(f"Entry {entry_id} deleted")
     return {"message": f"Entry {entry_id} deleted"}
