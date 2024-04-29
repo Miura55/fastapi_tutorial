4156377c452e777c1851772dada9fc05df70b131
Miura55 <acordion.piano@gmail.com>
2024-04-29 03:21:15 +0000

    SQLAlchemyの導入とモデルの導入

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from .database import Base
+
+
+class Entry(Base):
+    __tablename__ = "entries"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    title = Column(String)
+    content = Column(String)
+    user_name = Column(String)
+    created_at = Column(DateTime)
