f2ef517094e273f79f0432fe9487905778bd58e1
Miura55 <acordion.piano@gmail.com>
2024-04-30 14:27:55 +0000

    データベースファイルの拡張子を変更

--- database.py
+++ database.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///./database.db")
+engine = create_engine("sqlite:///./database.sqlite")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
